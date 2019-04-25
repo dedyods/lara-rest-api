bc37247e9438351a317b8acddda9754d63deff98
dedyods <dedysetiawan1986@gmail.com>
2019-04-25 23:38:30 +0700

    Update index.blade.php
    upload laradss

--- resources/views/parameter/index.blade.php
+++ resources/views/parameter/index.blade.php
@@ -3,14 +3,13 @@
 @section('content')
 <!-- awal row -->
 <div class="row">
-<div class="col-lg-6">
-
+  <div class="col-lg-6">
               <!-- Default Card Example -->
-              <div class="card shadow mb-4">
+    <div class="card shadow mb-4">
                 
-                <div class="card-header">
+        <div class="card-header">
                   
-                  <div style="display: inline-block;>
+              <div style="display: inline-block;>
                    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>Tambah Data</a>
                   </div>
                  <div style="padding-top:0px; display:inline-block; padding-left:200px;"> PARAMETER 1</div>
@@ -48,114 +47,153 @@
                       <td>2</td>
                       <td>Baik</td>
                       <td>0,25</td>
-                      <td>Edit | Delete</i></td>
+                      <td>
+                        <a href="#" class="btn btn-danger btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                  <a href="#" class="btn btn-warning btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                
+                </td>
                       
                     </tr>
                     <tr>
                       <td>3</td>
                       <td>Cukup</td>
                       <td>0,25</td>
-                      <td>Edit | Delete</i></td>
+                      <td>
+                        <a href="#" class="btn btn-danger btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                  <a href="#" class="btn btn-warning btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                
+                </td>
                     </tr>
                     <tr>
                       <td>4</td>
                       <td>Kurang</td>
                       <td>0,25</td>
-                      <td>Edit | Delete</i></td>
+                      <td>
+                        <a href="#" class="btn btn-danger btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                  <a href="#" class="btn btn-warning btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                
+                </td>
                     </tr>
                    
                   </tbody>
                 </table>
-              </div>
-                </div>
-              </div>
+      </div>
+    </div>
+  </div>
 </div>
 
- <div class="col-lg-6">
-
-             <div class="card shadow mb-4">
-                <div class="card-header">
-                 <div style="display: inline-block;>
+  <div class="col-lg-6">
+              <!-- Default Card Example -->
+    <div class="card shadow mb-4">
+                
+        <div class="card-header">
+                  
+              <div style="display: inline-block;>
                    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>Tambah Data</a>
                   </div>
                  <div style="padding-top:0px; display:inline-block; padding-left:200px;"> PARAMETER 2</div>
                 </div>
                 <div class="card-body">
-                  This card uses Bootstrap's default styling with no utility classes added. Global styles are the only things modifying the look and feel of this default card example.
-                </div>
-              </div>
-</div>
+                  <div class="table-responsive">
+                <table class="table table-bordered"  width="50%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Rentan Parameter</th>
+                      <th>Bobot</th>
+                      <th><i class="fas fa-wrench" style="padding-left:40px"></i></th>
+                    
+                    </tr>
+                  </thead>
+                  
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Sangat Baik</td>
+                      <td>0,75</td>
+                      <td>
+                        <a href="#" class="btn btn-danger btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                  <a href="#" class="btn btn-warning btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                
+                </td>
+                      
+                    </tr>
+                     <tr>
+                      <td>2</td>
+                      <td>Baik</td>
+                      <td>0,25</td>
+                      <td>
+                        <a href="#" class="btn btn-danger btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                  <a href="#" class="btn btn-warning btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                
+                </td>
+                      
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Cukup</td>
+                      <td>0,25</td>
+                      <td>
+                        <a href="#" class="btn btn-danger btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                  <a href="#" class="btn btn-warning btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                
+                </td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Kurang</td>
+                      <td>0,25</td>
+                      <td>
+                        <a href="#" class="btn btn-danger btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                  <a href="#" class="btn btn-warning btn-circle">
+                    <i class="fas fa-trash"></i>
+                  </a></i>
+                
+                </td>
+                    </tr>
+                   
+                  </tbody>
+                </table>
+      </div>
+    </div>
+  </div>
 </div>
 
 <!-- awal row -->
-<div class="row">
-<div class="col-lg-6">
 
-              <!-- Default Card Example -->
-              <div class="card shadow mb-4">
-                <div class="card-header">
-                 <div style="display: inline-block;>
-                   <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>Tambah Data</a>
-                  </div>
-                 <div style="padding-top:0px; display:inline-block; padding-left:200px;"> PARAMETER 3</div>
-                </div>
-                <div class="card-body">
-                  This card uses Bootstrap's default styling with no utility classes added. Global styles are the only things modifying the look and feel of this default card example.
-                </div>
-              </div>
-</div>
 
- <div class="col-lg-6">
-
-             <div class="card shadow mb-4">
-                <div class="card-header">
-                 <div style="display: inline-block;>
-                   <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>Tambah Data</a>
-                  </div>
-                 <div style="padding-top:0px; display:inline-block; padding-left:200px;"> PARAMETER 4</div>
-                </div>
-                <div class="card-body">
-                  This card uses Bootstrap's default styling with no utility classes added. Global styles are the only things modifying the look and feel of this default card example.
-                </div>
-              </div>
-</div>
-</div>
-
-<!-- awal row 12-->
-<div class="row">
-<div class="col-lg-12">
-
-              <!-- Default Card Example -->
-              <div class="card shadow mb-4">
-                <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
-                  <h6 class="m-0 font-weight-bold text-primary">Collapsable Card Example</h6>
-                </a>
-                <!-- Card Content - Collapse -->
-                <div class="collapse show" id="collapseCardExample">
-                <div class="card-body">
-                  This card uses Bootstrap's default styling with no utility classes added. Global styles are the only things modifying the look and feel of this default card example.
-                </div>
-              </div>
-</div>
-<div class="row">
-<div class="col-lg-12">
-
-              <!-- Default Card Example -->
-              <div class="card shadow mb-4">
-                <a href="#collapseCardExample2" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
-                  <h6 class="m-0 font-weight-bold text-primary">Collapsable Card Example</h6>
-                </a>
-                <!-- Card Content - Collapse -->
-                <div class="collapse show" id="collapseCardExample2">
-                <div class="card-body">
-                  This card uses Bootstrap's default styling with no utility classes added. Global styles are the only things modifying the look and feel of this default card example.
-                </div>
-              </div>
-</div>
-</div>
+ 
 </div>
 
 
 
 
 @endsection
+
+
